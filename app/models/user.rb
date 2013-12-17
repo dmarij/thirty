f4d943f657354bee9d5cd9498cf2c389a74de4f3
dmarij <dmarijancic@gmail.com>
2013-12-17 16:32:01 +0100

    add default role after signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
+	after_create :set_default_role
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  private
+  	def set_default_role
+  		self.roles << Role.find_by_name(:user)
+  		self.save
+  	end
 end

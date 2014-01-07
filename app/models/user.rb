ffc1bbcb23a9952bff9d3f0532b62a5ddccf9ddf
dmarij <dmarijancic@gmail.com>
2014-01-07 19:43:28 +0100

    fix layout for mobile

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,8 @@ class User < ActiveRecord::Base
   before_create :set_defaults
   private
   	def set_defaults
-      # self.update_columns(roles: Role.find_by_name(:user), notes_order: 'desc', notes_order_inline: 'desc')
   		self.roles << Role.find_by_name(:user)
       self.notes_order = 'desc'
       self.notes_order_inline = 'desc'
-    #   self.save
   	end
 end
